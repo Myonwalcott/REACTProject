88c974aa287b9db11e9870d3fd5d040b3eed69d1
Myon Section D <N01660463@humber.ca>
2024-11-26 17:35:08 -0400

    added pictures to input into the code

--- dwive/src/App.js
+++ dwive/src/App.js
@@ -1,11 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
-import Navbar from './components/Navbar';
-import CheckoutForm from './components/CheckoutForm';
-import Footer from './components/Footer';
-import Login from './components/Login';
-import './App.css'; 
-=======
 import Navbar from './components/navbar';
 import CheckoutForm from './components/checkoutform';
 import Footer from './components/footer';
@@ -14,21 +7,16 @@ import Search from './components/search';
 import PopularDestinations from './components/populardestinations';
 import './index.css'; // Global styles
 import './App.css';   // Application-wide styles
->>>>>>> 95fb13b84cd50f409b5530880abf69e246b67e8f
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-<<<<<<< HEAD
-      <Login /> 
-=======
       <header className="App-header">
         <Hero />
         <Search />
         <PopularDestinations />
       </header>
->>>>>>> 95fb13b84cd50f409b5530880abf69e246b67e8f
       <CheckoutForm />
       <Footer />
     </div>
