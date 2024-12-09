9c68b67943cd9744666226d7c03ca94522bed664
Arwa-sectionD <n01709742@humber.ca>
2024-12-09 15:39:17 -0500

    fixed duplicate nav and login

--- dwive/src/components/login.js
+++ dwive/src/components/login.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import './styles/login.css';
-import Navbar from './navbar';
 import { Link } from 'react-router-dom';
+import './styles/login.css';
 
 const Login = () => {
   const showSignUpAlert = () => {
@@ -13,31 +12,23 @@ const Login = () => {
   };
 
   return (
-    <div>
-      <Navbar />
-      <div className="flex-container">
-        <div className="flex-item">
-          <h4>Email</h4>
-          <input type="email" className="form-control" placeholder="name@example.com" />
-        </div>
-        <div className="flex-item">
-          <h4>Password</h4>
-          <input type="password" className="form-control" placeholder="password" />
-        </div>
-        <div className="button-container">
-          <Link to="/signup" className="button" onClick={showSignUpAlert}>
-            Sign Up
-          </Link>
-          <Link to="/login" className="button" onClick={showLogINAlert}>
-            Log In
-          </Link>
-        </div>
+    <div className="flex-container">
+      <div className="flex-item">
+        <h4>Email</h4>
+        <input type="email" className="form-control" placeholder="name@example.com" />
       </div>
-      <footer className="bg-light text-center text-lg-start mt-5">
-        <div className="container p-4">
-          <p className="text-center">&copy; 2024 DWIVE Travel Co. All rights reserved.</p>
-        </div>
-      </footer>
+      <div className="flex-item">
+        <h4>Password</h4>
+        <input type="password" className="form-control" placeholder="password" />
+      </div>
+      <div className="button-container">
+        <Link to="/signup" className="button" onClick={showSignUpAlert}>
+          Sign Up
+        </Link>
+        <Link to="/login" className="button" onClick={showLogINAlert}>
+          Log In
+        </Link>
+      </div>
     </div>
   );
 };
