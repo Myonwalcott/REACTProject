28bb783220d0fc7d55f14fe7e4d9fb38574cbab5
Myon Section D <N01660463@humber.ca>
2024-12-10 02:19:36 -0400

    fixed app header issue but got a login page display error

--- dwive/src/components/Hero.js
+++ dwive/src/components/Hero.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './styles/hero.css';
 
 const Hero = () => {
   return (
--- dwive/src/components/login.js
+++ dwive/src/components/login.js
@@ -4,30 +4,40 @@ import './styles/login.css';
 
 const Login = () => {
   const showSignUpAlert = () => {
-    alert("You have successfully signed up!");
+    alert('You have successfully signed up!');
   };
 
   const showLogINAlert = () => {
-    alert("You have successfully logged in!");
+    alert('You have successfully logged in!');
   };
 
   return (
-    <div className="flex-container">
-      <div className="flex-item">
-        <h4>Email</h4>
-        <input type="email" className="form-control" placeholder="name@example.com" />
-      </div>
-      <div className="flex-item">
-        <h4>Password</h4>
-        <input type="password" className="form-control" placeholder="password" />
-      </div>
-      <div className="button-container">
-        <Link to="/signup" className="button" onClick={showSignUpAlert}>
-          Sign Up
-        </Link>
-        <Link to="/login" className="button" onClick={showLogINAlert}>
-          Log In
-        </Link>
+    <div className="login-page">
+      <div className="flex-container">
+        <div className="flex-item">
+          <h4>Email</h4>
+          <input
+            type="email"
+            className="form-control"
+            placeholder="name@example.com"
+          />
+        </div>
+        <div className="flex-item">
+          <h4>Password</h4>
+          <input
+            type="password"
+            className="form-control"
+            placeholder="password"
+          />
+        </div>
+        <div className="button-container">
+          <Link to="/signup" className="button" onClick={showSignUpAlert}>
+            Sign Up
+          </Link>
+          <Link to="/login" className="button" onClick={showLogINAlert}>
+            Log In
+          </Link>
+        </div>
       </div>
     </div>
   );
